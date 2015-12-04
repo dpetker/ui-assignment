0ed221d4c5a048cfdbdb4dae10388faeb88eb21c
David Wilhelm <dewilhelm@gmail.com>
2015-12-04 00:06:50 -0500

    add bootstrap css and fonts manually, update js build

--- build.js
+++ build.js
@@ -1,6 +1,6 @@
 var jspm = require('jspm');
 
 //create self-executing bundle
-jspm.bundleSFX('js/main.js', 'bundle.js', { mangle: false }).then(function () {
+jspm.bundleSFX('js/main.js', 'bundle.js', { mangle: false, separateCSS: true }).then(function () {
     console.log('built bundle.js');
 });
--- js/CommitList.js
+++ js/CommitList.js
@@ -9,17 +9,26 @@ export default class CommitList {
         `<tr class="commit ${idx%2 === 0 ? 'even' : 'odd'}" id="${commit.sha}">
               <td class="author-name">${commit.author.login}</td>
               <td class="time">${this.formatDateTime(commit.commit.committer.date)}</td>
-              <td class="msg" title="${commit.commit.message}">${this.formatMessage(commit.commit.message)}</td>
+              <td class="msg" >${this.formatMessage(commit.commit.message)}</td>
               <td class="additions">+${(commit.stats.additions)}</td>
               <td class="deletions">-${(commit.stats.additions)}</td>
               <td class="files">${this.formatFiles(commit.files)}</td>
-
          </tr>`
 
         this.pageSize = 25;
+        this.tmpEl = document.createElement("div");    
 
         this.render(0, this.pageSize);
 
+        $(".commit-message").click((e) => {
+            let msg = $(e.target).attr('title');
+            $("#commit-msg-input").val(msg);
+        });
+
+        $(".patch-link").click((e) => {
+            let patch = $(e.target).data('content');
+            $("#patch-content").text(patch);
+        });
     }
 
     render(start=0, end=25) {
@@ -28,6 +37,7 @@ export default class CommitList {
             .map(this.itemTemplate).join("");
 
         this.$el.html(html);
+
     }
 
     formatDateTime(dateStr) {
@@ -35,22 +45,38 @@ export default class CommitList {
     }
 
     formatMessage(msg) {
+        let message = msg;
         if (msg.length > 50) {
-            return msg.substr(0, 47) + "...";
-        } else {
-            return msg;
-        }
+            message = msg.substr(0, 47) + "...";
+        }  
+        return `<a title="${msg}"
+                   class="commit-message"
+                   href="#" role="button"
+                   data-toggle="modal"
+                   data-target="#commit-msg-modal">${message}</a>`;
+    }
+
+    formatPatch(patchStr) {
+        this.tmpEl.innerHTML = patchStr;
+        return this.tmpEl.innerText;
     }
 
     formatFiles(files) {
-    
-        return files.map(function (f) {
+        return files.map(f => {
             if (f.patch) {
-                return `<a href="#" class="patch-link" >${f.filename}</a>`;
+                return `<a href="#" class="patch-link" role="button"
+                data-toggle="modal"
+                title="Patch for ${f.filename}"
+                data-placement="top" 
+                data-trigger="focus"
+                data-target="#patch-modal"
+                data-content="${this.formatPatch(f.patch)}"
+                >${f.filename}</a>`;
             } else {
                 return f.filename;
             }
         }).join(" ");
     }
+
 
 }
